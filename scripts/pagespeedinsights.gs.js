cb536a8389b6ac2090e5ab53e7f1b8a42a0fd62e
IronistM <m.parzakonis@statsravingmad.com>
2015-04-14 22:17:53 +0300

    Use v2 of the API

--- scripts/pagespeedinsights.gs.js
+++ scripts/pagespeedinsights.gs.js
@@ -1,35 +1,46 @@
 function pageSpeedInsights(url,device,filter_third_party_resources,http_secure) {
-  
+
   url = url || 'www.statsravingmad.com';
-  strategy = 'desktop' || device; // 'desktop' or 'mobile'. 
+  strategy = 'desktop' || device; // 'desktop' or 'mobile'.
   filter_third_party_resources = 'true' || filter_third_party_resources;
   http_secure = 'false' || http_secure ; // if it SSL type in "true".
   Logger.log(http_secure); // for test runs. comment it out if you like. See logs using Ctrl + Enter.
-  
+
   // Create a protocol parameter to pass to the GET URL
   switch (http_secure)  {
-        case 'false':
+    case 'false':
       http_protocol = 'http://';
-            break;
-        case 'true':
-      http_protocol = 'https://';
-            break;
-    }
-  
+      break;
+      case 'true':
+        http_protocol = 'https://';
+        break;
+      }
+
   Logger.log(http_protocol); // for test runs. comment it out if you like
-  
-  var key = 'YOUR API KEY';     // Get the API key from Google Dev Console
-  var api = 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=' + http_protocol + url
-                + '&filter_third_party_resources=' + filter_third_party_resources + '&strategy=' + strategy + '&key=' + key;
-  
+
+  var key = 'AIzaSyBJW16KnBT_xILW5X6tdyqVB6SoYGkjlQA';     // Get the API key from Google Dev Console
+  var api = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=' + http_protocol + url
+  + '&filter_third_party_resources=' + filter_third_party_resources + '&strategy=' + strategy + '&key=' + key;
+
   Logger.log(api); // for test runs. comment it out if you like
   Logger.log(url); // for test runs. comment it out if you like
-  
-  var response = UrlFetchApp.fetch(api, {muteHttpExceptions: true });
-  
-  var result = JSON.parse(response.getContentText()); // yeap, it is JSON
 
-  score = result.score;
+  var response = UrlFetchApp.fetch(api, {muteHttpExceptions: true });
+
+  var result = JSON.parse(response.getContentText()); // yeap, it is JSON
+  
+  // Example of JSON in order to formulate the score below
+  // "kind": "pagespeedonline#result",
+  // "id": "http://statsravingmad.com/",
+  // "responseCode": 200,
+  // "title": "Stats Raving Mad",
+  // "ruleGroups": {
+  //   "SPEED": {
+  //     "score": 75
+  //   }
+  // },
+
+  score = result.ruleGroups.SPEED.score;
   Logger.log(score); // for test runs. comment it out if you like
 
   return(score);
